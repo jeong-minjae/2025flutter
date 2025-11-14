8ea903fa44e9e33d847335a95b379c421a19b820
jeong-minjae <wjdalswo57@gmail.com>
2025-11-14 18:14:49 +0900

    Add files via upload

---
+++ lib/secondpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Secondpage extends StatelessWidget {
+  const Secondpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+      ),
+      body: Center(
+        child: Text("2번째 페이지입니다"),
+      ),
+    );
+  }
+}
